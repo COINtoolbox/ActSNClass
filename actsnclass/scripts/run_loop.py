ab451e78f0c2e15dada6b53ff9151a60980c8249
emilleishida <emilleishida@gmail.com>
2019-08-11 22:41:53 +0200

    add DataBase object

--- actsnclass/scripts/run_loop.py
+++ actsnclass/scripts/run_loop.py
@@ -57,9 +57,9 @@ def main(args):
 
     Run directly from the command line:
 
-    >>> run_loop.py -b <batch size> -d <metrics file> -i <features file>
-    >>>             -n <number of loops> -q <queried sample file>
-    >>>             -s <learning strategy> -t <choice of training>
+   >>> run_loop.py -i <input features file> -b <batch size> -n <number of loops>
+   >>>             -d <output metrics file> -q <output queried sample file>
+   >>>             -s <learning strategy> -t <choice of initial training>
 
     """
 
