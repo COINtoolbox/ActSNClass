aa53baded19f7fa37c3981d7701a2b940f69ccd6
emilleishida <emilleishida@gmail.com>
2020-03-19 21:17:51 +0100

    add module for cosmology metric

---
+++ actsnclass/cosmo_metric.py
@@ -0,0 +1,45 @@
+# Copyright 2020 snactclass software
+# Author: Mi Dai
+#         
+# created on 19 March 2019
+#
+# Licensed GNU General Public License v3.0;
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import
+
+
+
+__all__ = ['get_distances']
+
+def get_distances(obj_list: list, ...):
+    """Calculates distances and erros for cosmology metric.
+
+    Parameters
+    ----------
+    obj_list: list
+       list
+
+    Returns
+    -------
+    array of (redshift, mu, muerr, prob (if possible))
+    """
+
+
+
+
+
+def main():
+        return None
+
+if __name__ == '__main__':
+   main()
