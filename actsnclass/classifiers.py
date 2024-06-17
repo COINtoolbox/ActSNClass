5e91fbde59645bc690207c5696ab17027e1f61e7
fedora Cloud User <fedora@fink-dev.lal.in2p3.fr>
2024-06-17 09:21:28 +0000

    Mlflow Tracking

--- actsnclass/classifiers.py
+++ actsnclass/classifiers.py
@@ -20,7 +20,8 @@ __all__ = ['random_forest']
 
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-
+import mlflow
+import mlflow.sklearn
 
 def random_forest(train_features:  np.array, train_labels: np.array,
                   test_features: np.array, nest=1000, seed=42, max_depth=None,
@@ -55,11 +56,38 @@ def random_forest(train_features:  np.array, train_labels: np.array,
     """
 
     # create classifier instance
-    clf = RandomForestClassifier(n_estimators=nest, random_state=seed,
+    '''    clf = RandomForestClassifier(n_estimators=nest, random_state=seed,
                                  max_depth=max_depth, n_jobs=n_jobs)
     clf.fit(train_features, train_labels)                     # train
     predictions = clf.predict(test_features)                # predict
     prob = clf.predict_proba(test_features)       # get probabilities
+    '''
+
+    # Initialiser MLflow
+    mlflow.start_run()
+
+    try:
+        # Enregistrer les hyperparamètres
+        mlflow.log_param("n_estimators", nest)
+        mlflow.log_param("random_state", seed)
+        mlflow.log_param("max_depth", max_depth)
+        mlflow.log_param("n_jobs", n_jobs)
+
+        # Créer une instance du classificateur
+        clf = RandomForestClassifier(n_estimators=nest, random_state=seed,
+                                     max_depth=max_depth, n_jobs=n_jobs)
+        clf.fit(train_features, train_labels)  # Entraîner le modèle
+
+        # Prédire
+        predictions = clf.predict(test_features)
+        prob = clf.predict_proba(test_features)  # Obtenir les probabilités
+
+        
+        # Enregistrer le modèle
+        mlflow.sklearn.log_model(clf, "random_forest_model")
+
+    finally:
+        mlflow.end_run()
 
     return predictions, prob
 
